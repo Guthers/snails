a5fbefacbc4108dd3ed1c208e90ee3601fe10093
Leo Orpilla III <leoorpilla3@gmail.com>
2020-08-11 13:41:01 +1000

    Add frontend skeleton

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="h-screen bg-black grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-4">
+      <div className="bg-red-200">
+      </div>
+      <div className="bg-red-400">
+      </div>
+      <div className="bg-green-200">
+      </div>
+      <div className="bg-green-400">
+      </div>
+    </div>
+  );
+}
+
+export default App;
