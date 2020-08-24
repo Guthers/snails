5f6b49f2681ef020c724d10b0fe969cd9b065219
Harry Guthrie <gtguthers@gmail.com>
2020-08-24 10:46:11 +1000

    Re-add port set for manual run

--- backend/app.py
+++ backend/app.py
@@ -18,5 +18,10 @@ def create_app():
 
 
 app = create_app()
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    parser = ArgumentParser()
+    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
+    args = parser.parse_args()
+    port = args.port
+    app.run(host='0.0.0.0', port=port)
