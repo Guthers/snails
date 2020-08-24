c3aae4d7e7db7cde55d8cd55c00a7dfb2e1c292f
Harry Guthrie <gtguthers@gmail.com>
2020-08-24 10:45:40 +1000

    Fix for better handelling for hosting for uwsgi

--- backend/README.md
+++ backend/README.md
@@ -4,6 +4,10 @@
 ## Project Structure
 ### Setup
 Setup the project by running `pipenv install`. This should install any packages needed
+
+Harry has some issues, it was a bit picky
+    I had to add the alternatives for python->python3 and pip->pip3
+
 ### Start
 `pipenv run python -m flask run`
 Visit http://localhost/api for the home api
--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flasgger import Swagger
 from api.route.home import home_api
+from argparse import ArgumentParser
 
 
 def create_app():
@@ -16,14 +17,6 @@ def create_app():
     return app
 
 
+app = create_app()
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
-
-    app = create_app()
-
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0')
