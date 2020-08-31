08a2b51df67e5352ff46e7b3b753b90a31bdcf91
Michael Delmastro <42367211+Delmastro@users.noreply.github.com>
2020-08-31 12:22:49 +1000

    Update DBschema.sql

--- database files/DBschema.sql
+++ database files/DBschema.sql
@@ -1,34 +1,57 @@
-/*Tables for the database: USER, ENTRY (POSTS), MESSAGE*/
+/*Tables for the database: USER, ENTRY (POSTS), MESSAGE, REPLY, TO*/
+
+/*Specifications: 
+Get messages of a user via userID
+Get posts of a user via userID 
+getUserResponses VARCHAR(225 BYTE),     //Get all the users who wrote a response
+LikedBy VARCHAR(225 BYTE)              //Get all the users who liked the post
+*/
 
 
-/*Creates a table to user informtation*/
+/*Table that stores user informtation*/
 CREATE TABLE USER (
   studentID INT PRIMARY KEY,
   name VARCHAR(225 BYTE),
-  username VARCHAR(225 BYTE),
-  getMessages VARCHAR(225 BYTE),
-  userCreateDate VARCHAR(225 BYTE),
+  bio VARCHAR(225 BYTE),
+  userCreateDate VARCHAR(225 BYTE)
 );
 
-/*Creates a table to store an entry (post) information*/
-CREATE TABLE ENTRY(
+/*Table that stores an entry/post information*/
+CREATE TABLE ENTRY (
   postID INT PRIMARY KEY,
   postContent VARCHAR(225 BYTE),
-  createDate VARCHAR(225 BYTE),
-  getAuthorID INT,
-  getAuthor VARCHAR(225 BYTE),         //Get the author of the post
-  getUserResponses VARCHAR(225 BYTE),  //Get all the users who wrote a response
-  LikedBy VARCHAR(225 BYTE),           //Get all the users who liked the post
+  createDate DATE,
+  getAuthorID INT,                        //Get the authorID of the post
+  getAuthor VARCHAR(225 BYTE),           
 );
 
 
-/*Creates a table to store private messaging information*/
+/*Table to store private messaging information*/
 CREATE TABLE MESSAGE (
   messageID INT PRIMARY KEY;
   messageContent VARCHAR(225 BYTE),
-  createDate VARCHAR(225 BYTE),
-  getFromUserID VARCHAR(225 BYTE),        //Get the user who sent the message
-  getToUserID VARCHAR(225 BYTE)           //Get the user who received the message
+  createDate DATE,
+  FromUserID INT,                        //Get userID who sent the message
+  ToUserID INT,                          //Get userID who received the message
+  FromUserName VARCHAR(225 BYTE),        //Get userName who sent the message
+  ToUserName VARCHAR(225 BYTE)           //Get userName who received the message
+);
+
+/*Table to store a reply:
+This allows us to get all responses to a post*/
+CREATE TABLE REPLY (
+  postID INT,
+  userID INT,
+  createDate DATE,
+  RepliedToName VARCHAR(225 BYTE)
+);
+
+/*Table to store a reply:
+This allows us to get all people who liked a post*/
+CREATE TABLE LIKED (
+  postID INT,
+  UserID INT,
+  createDate DATE
 );
 
 
