1ddc6348833716bb3aa957edeffda9f3a6f89e6b
Leo Orpilla III <leoorpilla3@gmail.com>
2020-09-07 10:39:10 +1000

    Add information Bar component

--- frontend/src/Bar.tsx
+++ frontend/src/Bar.tsx
@@ -7,16 +7,18 @@ type Props = {
   weather: string,
 }
 
-function Bar(props: Props) {
+/**
+ * An information bar showing time, date, temperature and weather
+ * @param props Props
+ */
+const Bar: React.FC<Props> = (props) => {
   return (
-  <div>
     <div className="font-black text-3xl font-mono text-center lg:text-left bg-black bg-opacity-50 p-3 px-5 tracking-tight">
       <span className="mr-6">{props.time}</span>
       <span className="mr-6">{props.date}</span>
       <span className="mr-6">{props.temperature}</span>
       <span className="mr-6">{props.weather}</span>
     </div>
-  </div>
   );
 }
 
