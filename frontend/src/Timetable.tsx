c57de24ab6baff9140f430b9d1bc3851711abd9c
Leo Orpilla III <leoorpilla3@gmail.com>
2020-09-07 10:39:54 +1000

    Improve bus Timetable component

--- frontend/src/Timetable.tsx
+++ frontend/src/Timetable.tsx
@@ -1,53 +1,94 @@
-import React from 'react';
+import React, { useState, useRef, useLayoutEffect, cloneElement, Children, isValidElement } from 'react';
+import {LAKES, CHANCELLORS} from './data';
 
-function Timetable() {
-  const LAKES = [
-    ["66", "ARR"],
-    ["28", "ARR"],
-    ["169", "2 min"],
-    ["169", "3 min"],
-    ["29", "10 min"],
-  ];
-  const CHANCELLORS = [
-    ["412", "1 min"],
-    ["402", "5 min"],
-    ["411", "6 min"],
-    ["432", "10 min"],
-    ["427", "15 min"],
-  ];
+const Timetable: React.FC = () => {
   return (
-  <div className="flex flex-col text-center">
+  <div className="flex flex-grow flex-col overflow-hidden">
     <h1 className="text-5xl font-bold text-center tracking-tight">Timetable</h1>
-    <div className ="flex-grow grid grid-rows-2">
-
-      <div>
-      <h2 className="text-3xl font-bold">UQ Lakes</h2>
-      {
-        LAKES.map(([x, y], i) => {
-          return <TimetableEntry routeCode={x} eta={y}/>
-        })
-      }
-      </div>
-      <div>
-      <h2 className="text-3xl font-bold">Chancellor's Place</h2>
-      {
-        CHANCELLORS.map(([x, y], i) => {
-          return <TimetableEntry routeCode={x} eta={y}/>
-        })
-      }
-      </div>
-      </div>
+    <ListContext>
+      <List 
+        title="Lakes" 
+        elements={LAKES} 
+        titleClass="text-3xl text-center font-bold" 
+        elementsClass="flex justify-between text-3xl"/>
+      <List 
+        title="Chancellors Place" 
+        elements={CHANCELLORS} 
+        titleClass="text-3xl text-center font-bold" 
+        elementsClass="flex justify-between text-3xl"/>
+    </ListContext>
   </div>
   );
 }
 
-type TimetableEntryProps = {
+type ListContextProps = {
+  children: React.ReactElement<ListProps>[];
+  className?: string;
+}
+
+const ListContext: React.FC<ListContextProps> = (props) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
+  const [numChildren, setNumChildren] = useState(0);
+
+  useLayoutEffect(() => {
+    if(ref.current) {
+      setHeight(ref.current.clientHeight);
+      setNumChildren(ref.current!.childElementCount)
+    }
+
+    const handleWindowResize = () => {
+      if(ref.current) {
+        setHeight(ref.current.getBoundingClientRect().height);
+      }
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, [height, numChildren]);
+
+  return (
+    <div ref={ref} className={"h-full " + props.className}>
+      {
+      Children.map(props.children, child =>
+          isValidElement(child) ? cloneElement(child, {items: Math.floor(height/numChildren/39) - (child.props.title ? 1 : 0)}) : null
+      )
+      }
+    </div>
+  )
+}
+
+type ListProps = {
+  className?: string,
+  title?: string,
+  titleClass?: string,
+  elements:[string, string][],
+  elementsClass?: string,
+  items?: number,
+}
+
+const List: React.FC<ListProps> = (props) => {
+  return <div className={props.className}>
+    {
+      props.title ? <h2 className={props.titleClass}>{props.title}</h2> : null
+    }
+    {
+      props.elements.slice(0, props.items).map(([routeCode, eta], i) => {
+          return <ListItem className={props.elementsClass} routeCode={routeCode} eta={eta} key={i}/>
+      })
+    }
+  </div>
+}
+
+type ListItemProps = {
   routeCode: string,
+  className?: string,
   eta: string,
 }
 
-function TimetableEntry(props: TimetableEntryProps) {
-  return <div className="flex justify-between text-3xl">
+const ListItem: React.FC<ListItemProps> = (props) => {
+  return <div className={props.className}>
     <span className="font-black">{props.routeCode}</span>
     <span className="font-black">{props.eta}</span>
   </div>
