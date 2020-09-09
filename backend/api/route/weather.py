3eac0b0b6bc93808d212ebbdf382fea38eb6e1a4
Joshua <joshua.hwang@ieee.org>
2020-09-09 15:56:05 +1000

    created a working (but very inefficient) weather backend

--- backend/api/route/weather.py
+++ backend/api/route/weather.py
@@ -3,6 +3,9 @@ from flasgger import swag_from
 import api.model as models
 from .api_register import api_register
 
+import datetime
+import urllib.request
+import xmltodict
 
 @api_register.route('/weather', methods=["GET"])
 @swag_from({
@@ -15,5 +18,40 @@ from .api_register import api_register
     }
 })
 def weather():
-    result = models.WeatherModel()
+    result = get_bom()
     return models.WeatherModel.schema()().jsonify(result), 200
+
+def get_bom() -> models.WeatherModel:
+    # hardcoded since won't change
+    url = 'ftp://ftp.bom.gov.au/anon/gen/fwo/IDQ11295.xml'
+
+    # making a request to bom everytime is probably not okay. Save file maybe?
+    with urllib.request.urlopen(url) as req:
+        # structure of bom xml sheet is hardcoded
+        info = xmltodict.parse(req.read())["product"]["forecast"]["area"]
+
+    # locate Brisbane
+    info = next(filter(lambda i: i["@description"] == "Brisbane", info))
+    # get first forecast period
+    info = info["forecast-period"][0]
+
+    created_at = datetime.datetime.strptime(info["@start-time-local"],
+            "%Y-%m-%dT%H:%M:%S+10:00")
+
+    current_temperature = int(next(filter(lambda i: i["@type"] ==
+            "air_temperature_maximum", info["element"]))["#text"])
+
+    precipitation = next(filter(lambda i: i["@type"] ==
+            "precipitation_range", info["element"]), None)
+    if precipitation:
+        # VERY hardcoded but oh well. Should make flexible later
+        precipitation = precipitation["#text"].split(' ')
+        precipitation = (int(precipitation[0]) + int(precipitation[2]))/2
+
+    # converts condition into PascalCase
+    conditions = ''.join(x for x in next(filter(lambda i: i["@type"] ==
+        "precis", info["text"]))["#text"][:-1].title() if not x.isspace())
+
+    return models.WeatherModel(created_at=created_at,
+            current_temperature=current_temperature,
+            precipitation=precipitation, conditions=conditions)
