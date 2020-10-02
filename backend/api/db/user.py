03e90e344ff0874fb3da5056798f66d5d14a4672
Joshua <joshua.hwang@ieee.org>
2020-10-02 18:12:57 +1000

    embarrassing typos

--- backend/api/db/user.py
+++ backend/api/db/user.py
@@ -1,7 +1,7 @@
 from .db import db
 
 class UserDB(db.Model):
-    __tablename__="user"
+    __tablename__="userdb"
     studentID = db.Column(db.String(255), primary_key = True)
     studentName = db.Column(db.String(255))
     bio = db.Column(db.String(255)) # pretty sure this doesn't work
