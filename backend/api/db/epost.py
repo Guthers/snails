876647ce5e4c00948491497c5e09e00cbec341b4
Joshua <joshua.hwang@ieee.org>
2020-10-09 14:17:55 +1000

    entry endpoints functional

--- backend/api/db/epost.py
+++ backend/api/db/epost.py
@@ -8,7 +8,6 @@ class Epost(db.Model):
     author_id = db.Column(db.String(255), db.ForeignKey('userdb.student_id'), nullable=False)
     content = db.Column(db.String(255))
     create_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    like_count = db.Column(db.Integer)
     likes = db.relationship("Liked", backref="epost", lazy=True)
     replies = db.relationship("Epost", cascade="all,delete",
             backref=db.backref("parent", remote_side=[post_id]), lazy=True)
--- backend/api/route/entry.py
+++ backend/api/route/entry.py
@@ -5,6 +5,7 @@ import api.model as models
 import api.db as dbs
 from .api_register import api_register
 
+import flask
 from flask import request
 from utils.route_utils import swag_param, PARAM_IN
 
@@ -50,7 +51,7 @@ def entry():
 
     # create an entry
     row = dbs.Epost(reply_id=replyTo, author_id=studentID, content=content,
-            create_date=datetime.now(), like_count=0)
+            create_date=datetime.now())
 
     dbs.db.session.add(row)
     dbs.db.session.commit()
@@ -80,7 +81,7 @@ def entry():
     return models.EntryModel.schema()().jsonify(result), 200
 
 
-@api_register.route('/entry/<int:entryID>', methods=["GET"])
+@api_register.route('/entry/<int:entryID>', methods=["GET","DELETE"])
 @swag_from({
     'tags': ['Entry'],
     'parameters': [{
@@ -97,29 +98,90 @@ def entry():
     }
 })
 def entry_id(entryID: int):
-    result = models.EntryModel()
+    entry = dbs.Epost.query.filter_by(post_id=entryID).first()
+    if entry is None:
+        return "entryID not found", 404
+
+    # append list of liked_by
+    liked_by = []
+    for like in entry.likes:
+        user = like.user
+        liked_by.append(models.UserModel(username=user.student_id,
+            name=user.student_name, user_id=user.student_id,
+            created_at=user.create_date))
+
+    replies = [r.post_id for r in entry.replies]
+
+    user = entry.author
+    author = models.UserModel(username=user.student_id,
+        name=user.student_name, user_id=user.student_id,
+        created_at=user.create_date)
+
+    result = models.EntryModel(created_at=entry.create_date,
+            reply_to=entry.reply_id,
+            content=entry.content, liked_by=liked_by,
+            replies=replies, author=author,
+            entry_id=entry.post_id)
+
+    if request.method == "DELETE":
+        studentID = request.headers.get("x-uq-user", None)
+        if studentID is None or studentID != user.student_id:
+            return "Invalid Authorization", 400
+        dbs.db.session.delete(entry)
+        dbs.db.session.commit()
+        
     return models.EntryModel.schema()().jsonify(result), 200
 
 
-@api_register.route('/entry/<int:entryID>/replies', methods=["GET"])
+@api_register.route('/entry/replies', methods=["GET"])
 @swag_from({
     'tags': ['Entry'],
-    'parameters': [{
-        'in': 'path',
-        'name': 'entryID',
-        'type': 'int',
-        'required': 'true'
-    }],
+    'parameters': [
+    swag_param(PARAM_IN.QUERY, "entryID", int),
+    ],
     'responses': {
         HTTPStatus.OK.value: {
             'description': 'Gets a list of entries which are replies to the entryID',
             'schema': models.EntryModel.schema()
+        },
+        400: {
+            'description': 'Invalid entryID',
         }
     }
 })
-def entry_id_replies(entryID: int):
-    result = models.EntryModel()
-    return models.EntryModel.schema()().jsonify(result), 200  # TODO Schema is wrong
+def entry_id_replies():
+    entryID = request.args.get("entryID", None)
+    if entryID is None or not entryID.isdigit():
+        return "Invalid entryID", 400
+
+    entry = dbs.Epost.query.filter_by(post_id=entryID).first()
+    if entry is None:
+        return "entryID not found", 404
+
+    result = []
+
+    for r in entry.replies:
+        liked_by = []
+        for like in r.likes:
+            user = like.user
+            liked_by.append({"username":user.student_id,
+                    "name":user.student_name, "user_id":user.student_id,
+                    "created_at":user.create_date})
+
+        replies = [rr.post_id for rr in r.replies]
+
+        user = r.author
+        author = { "username": user.student_id,
+                "name": user.student_name, "user_id": user.student_id,
+                "created_at": user.create_date }
+
+        result.append({"created_at": r.create_date,
+            "reply_to": r.reply_id,
+            "content": r.content, "liked_by": liked_by,
+            "replies": replies, "author": author,
+            "entry_id": r.post_id})
+
+    return flask.json.jsonify(result), 200
 
 
 @api_register.route('/entry/like/<int:entryID>', methods=["POST"])
@@ -135,13 +197,57 @@ def entry_id_replies(entryID: int):
         HTTPStatus.OK.value: {
             'description': 'Like the specified entry',
             'schema': models.EntryModel.schema()
-        }
+        },
+        400: {
+            'description': 'Invalid Authorization',
+        },
+        404: {
+            'description': 'entryID not found',
+        },
     }
 })
 def like_entry(entryID: int):
-    result = models.EntryModel()
-    return models.EntryModel.schema()().jsonify(result), 200  # TODO Schema is wrong
+    entry = dbs.Epost.query.filter_by(post_id=entryID).first()
+    if entry is None:
+        return "entryID not found", 404
 
+    studentID = request.headers.get("x-uq-user", None)
+    if not studentID:
+        return "Invalid Authorization", 400
+    # check studentID in database
+    if dbs.UserDB.query.filter_by(student_id=studentID).scalar() is None:
+        return "Invalid Authorization", 400
+
+    # check entry doesn't already exist
+    if dbs.Liked.query.filter_by(post_id=entryID, user_id=studentID).scalar() is None:
+        # create an entry
+        row = dbs.Liked(post_id=entryID, user_id=studentID)
+
+        dbs.db.session.add(row)
+        dbs.db.session.commit()
+
+    # append list of liked_by
+    liked_by = []
+    for like in entry.likes:
+        user = like.user
+        liked_by.append(models.UserModel(username=user.student_id,
+            name=user.student_name, user_id=user.student_id,
+            created_at=user.create_date))
+
+    replies = [r.post_id for r in entry.replies]
+
+    user = entry.author
+    author = models.UserModel(username=user.student_id,
+        name=user.student_name, user_id=user.student_id,
+        created_at=user.create_date)
+
+    result = models.EntryModel(created_at=entry.create_date,
+            reply_to=entry.reply_id,
+            content=entry.content, liked_by=liked_by,
+            replies=replies, author=author,
+            entry_id=entry.post_id)
+
+    return models.EntryModel.schema()().jsonify(result), 200
 
 @api_register.route('/entry/unlike/<int:entryID>', methods=["POST"])
 @swag_from({
@@ -160,26 +266,41 @@ def like_entry(entryID: int):
     }
 })
 def unlike_entry(entryID: int):
-    result = models.EntryModel()
-    return models.EntryModel.schema()().jsonify(result), 200  # TODO Schema is wrong
+    entry = dbs.Epost.query.filter_by(post_id=entryID).first()
+    if entry is None:
+        return "entryID not found", 404
 
+    studentID = request.headers.get("x-uq-user", None)
+    if not studentID:
+        return "Invalid Authorization", 400
+    # check studentID in database
+    if dbs.UserDB.query.filter_by(student_id=studentID).scalar() is None:
+        return "Invalid Authorization", 400
+    like = dbs.Liked.query.filter_by(post_id=entryID, user_id=studentID).first()
+    # check entry doesn't already exist
+    if like is not None:
+        dbs.db.session.delete(like)
+        dbs.db.session.commit()
 
-@api_register.route('/entry/<int:entryID>', methods=["DELETE"])
-@swag_from({
-    'tags': ['Entry'],
-    'parameters': [{
-        'in': 'path',
-        'name': 'entryID',
-        'type': 'int',
-        'required': 'true'
-    }],
-    'responses': {
-        HTTPStatus.OK.value: {
-            'description': 'Delete the entry by the specified id',
-            'schema': models.EntryModel.schema()
-        }
-    }
-})
-def delete_entry(entryID: int):
-    result = models.EntryModel()
-    return models.EntryModel.schema()().jsonify(result), 200  # TODO Schema is wrong
+    # append list of liked_by
+    liked_by = []
+    for like in entry.likes:
+        user = like.user
+        liked_by.append(models.UserModel(username=user.student_id,
+            name=user.student_name, user_id=user.student_id,
+            created_at=user.create_date))
+
+    replies = [r.post_id for r in entry.replies]
+
+    user = entry.author
+    author = models.UserModel(username=user.student_id,
+        name=user.student_name, user_id=user.student_id,
+        created_at=user.create_date)
+
+    result = models.EntryModel(created_at=entry.create_date,
+            reply_to=entry.reply_id,
+            content=entry.content, liked_by=liked_by,
+            replies=replies, author=author,
+            entry_id=entry.post_id)
+
+    return models.EntryModel.schema()().jsonify(result), 200
