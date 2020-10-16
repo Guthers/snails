0dabcd14b3c8d690a487a67b257a08a886806450
Joshua-Hwang <41476440+Joshua-Hwang@users.noreply.github.com>
2020-10-16 17:01:37 +1000

    added precision to datetime but had to use the specific mysql dialect

--- backend/api/db/entry.py
+++ backend/api/db/entry.py
@@ -1,11 +1,12 @@
 from .db import db
 import datetime
+from sqlalchemy.dialects.mysql import DATETIME
 
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(255))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    created_at = db.Column(DATETIME(fsp=6), default=datetime.datetime.utcnow)
     reply_id = db.Column(db.Integer, db.ForeignKey('entry.id'))
     likes = db.relationship("Liked", backref="entry", lazy=True)
     replies = db.relationship("Entry", 
