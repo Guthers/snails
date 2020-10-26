b1bbc8784574b911c917205effebc0b5dfab0ed0
Harry Guthrie <gtguthers@gmail.com>
2020-10-26 12:28:56 +1000

    Connects the bar to the backend

--- frontend-board/src/Bar.tsx
+++ frontend-board/src/Bar.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-
-type Props = {
-  time: string,
-  date: string,
-  temperature: string,
-  weather: string,
-}
-
-//TODO: Add api call
+import React, { useState, useEffect } from 'react';
+import { getBarProps } from './Api';
 
 /**
  * An information bar showing time, date, temperature and weather
  * @param props Props
  */
-const Bar: React.FC<Props> = (props) => {
+const Bar: React.FC = () => {
+  const [time, setTime] = useState("");
+  const [date, setDate] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [weather, setWeather] = useState("");
+
+  useEffect(() => {
+    getBarProps().then((response) => {
+      console.log(response);
+      response.json().then(data => {
+        setTime(data["created_at"].substr(0, 10));
+        setDate(data["created_at"].substr(11));
+        setTemperature(data["current_temperature"] ? data["current_temperature"] : "");
+        setWeather(data["conditions"]);
+      })
+    });
+  })
+
   return (
     <div className="font-black text-3xl font-mono text-center lg:text-left bg-black bg-opacity-50 p-3 px-5 tracking-tight">
-      <span className="mr-6">{props.time}</span>
-      <span className="mr-6">{props.date}</span>
-      <span className="mr-6">{props.temperature}</span>
-      <span className="mr-6">{props.weather}</span>
+      <span className="mr-6">{time}</span>
+      <span className="mr-6">{date}</span>
+      <span className="mr-6">{temperature}</span>
+      <span className="mr-6">{weather}</span>
     </div>
   );
 }
