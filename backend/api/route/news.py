96a57b0057a88c60084109b1c4499a84f9669bd9
Leo Orpilla III <leoorpilla3@gmail.com>
2020-10-29 20:28:13 +1000

    Fix news image_url

--- backend/api/route/news.py
+++ backend/api/route/news.py
@@ -2,6 +2,8 @@
 
 from .api_register import api_register
 
+from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 from flasgger import swag_from
 from functools import lru_cache
@@ -34,8 +36,7 @@ UQNEWS_RSS_URL = 'http://www.uq.edu.au/news/rss/news_feed.xml'
 def news():
     res = refresh_news(time() // 60 // 60)
     #return json.dumps(res, default=str), HTTPStatus.OK
-    # return NewsModel.schema()().jsonify(res), HTTPStatus.OK
-    return flask.json.jsonify(res), HTTPStatus.OK
+    return NewsModel.schema()().jsonify(res, many=True), HTTPStatus.OK
 
 @api_register.route('/news/<string:news_id>', methods=["GET"])
 @swag_from({
@@ -53,36 +54,42 @@ def news():
 def get_news(news_id: str):
     res = refresh_news(time() // 60 // 60)
     ret = next(filter(lambda x: x["news_id"] == news_id, res), None)
-    print(ret)
-    if ret:
-        # XXX create NewsModel then jsonify
-        news = NewsModel(**ret)
-        return NewsModel.schema()().jsonify(news), HTTPStatus.OK
-    else:
+    if not ret:
         return "news_id not found", HTTPStatus.NOT_FOUND
+    return NewsModel.schema()().jsonify(news), HTTPStatus.OK
+
+def parse_news(item):
+    created_at = datetime.strptime(item.get("pubDate"), "%a, %d %b %Y %H:%M:%S %z")
+    image_url = None
+
+    url = item.get("link")
+
+    req = urllib.request.Request(url)
+    req.add_header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0")
+    with urllib.request.urlopen(req) as res:
+        soup = BeautifulSoup(res.read(), 'html.parser')
+        img = soup.select('.uq-core-image img')
+        if len(img) >= 1:
+            image_url = img[0]['src']
+
+    news_id = ''.join(c for c in item.get("guid") if c.isdigit())
+    news = NewsModel(created_at=created_at, url=item.get("link"),
+                     content=item.get("description"), image_url=image_url,
+                     news_id=news_id, title=item.get("title"))
+    return news
 
 @lru_cache(maxsize=2)
 def refresh_news(t):
     '''rss fails'''
-
     req = urllib.request.Request(UQNEWS_RSS_URL)
     req.add_header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0")
+
     with urllib.request.urlopen(req) as rss:
         items = xmltodict.parse(rss.read())["rss"]["channel"]["item"]
 
-    ret = []
-    for i in items:
-        created_at = datetime.strptime(i.get("pubDate"), "%a, %d %b %Y %H:%M:%S %z")
-        image_url = None
-        news_id = ''.join(c for c in i.get("guid") if c.isdigit())
-        # XXX we're not using the NewsModel here since we need a list of such
-        # objects
-        #news = NewsModel(created_at=created_at, url=i.get("link"),
-        #        content=i.get("description"), image_url=image_url,
-        #        news_id=news_id, title=i.get("title"))
-        news = {"created_at":created_at, "url":i.get("link"),
-                "content":i.get("description"), "image_url":image_url,
-                "news_id":news_id, "title":i.get("title")}
-        ret.append(news)
+    with ThreadPoolExecutor() as executor:
+        ret = list(executor.map(parse_news, items))
 
     return ret
+
+refresh_news(time() // 60 // 60)
